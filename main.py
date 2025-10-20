661c88f9e9ca3608adbfa834d4f1abc75d927827
Lukas Hempel <lukas.hempel@bearingpoint.com>
2025-10-20 15:56:30 +0200

    Add PyLint on Push (#7)
    * Add PyLint on Push
    
    * Adjust to lint errors
    
    * Half required python version executions

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-
-import argparse
+""" This module enables me to start testing GitHub Actions and Dependabot """
 
 def main():
+    """ This function performs the main work necessary for the application """
     print("Hello, this is an enourmous test of Dependabot and GitHub Actions")
-
 
 # Starter
 if __name__ == '__main__':
